8734a00df78ebea25970c2be6c9bced30c2eb2cf
SplitSky <opop.neska60@gmail.com>
2026-02-04 22:29:58 +0000

    feat: Added the matching function and tested for limited cases. Fails at exact match.

---
+++ cron.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from typing import Iterable
+
+class CronSpec:
+    __slots__ = ("minute", "hour", "day", "month", "weekday")
+
+    def __init__(self, expression: str):
+        fields = expression.split()
+        if len(fields) != 5:
+            raise ValueError("Cron expressions must have 5 fields. Wrong format")
+        self.minute = self.parse_field(fields[0],0,59)
+        self.hour = self.parse_field(fields[1], 0, 23)
+        self.day = self.parse_field(fields[2], 1, 31)
+        self.month = self.parse_field(fields[3], 1, 12)
+        self.weekday = self.parse_field(fields[4], 0, 6)
+
+    def matches(self, dt: datetime) -> bool:
+        cron_weekday = (dt.weekday() + 1) % 1 # lil bit of modulus to fix the range issue
+        return bool((self.minute >> dt.minute) & 1 and (self.hour >> dt.hour) & 1 and (self.day >> dt.day) & 1 and (self.month >> dt.month) & 1 and (self.weekday >> dt.weekday()) & 1)
+    
+    def parse_field(self, field: str, min: int, max: int) -> int:
+        mask = 0
+        for part in field.split(","):
+            if part == "*":
+                return ((1 << (max - min + 1)) - 1) << min
+            
+            step = 1
+            if "/" in part:
+                part, step_str = part.split("/")
+                step = int(step_str)
+                if step <= 0:
+                    raise ValueError("Step must be pos")
+
+            if part == "*":
+                start, end = min, max
+            elif "-" in part:
+                start_str, end_str = part.split("-")
+                start, end = int(start_str), int(end_str)
+            else:
+                start = end = int(part)
+
+            if start < min or end > max or start > end:
+                raise ValueError("Value out of range")
+
+            for v in range(start, end + 1, step):
+                mask |= 1 << v
+
+        return mask
--- notes.md
+++ notes.md
@@ -3,7 +3,7 @@
 1. Setup initial bitmask cron parser
 2. Implement the silly OR logic for day of week/ day of month
 3. Check that the datetime days 0-6 1-7 ranges are consistent
-4.
+
 
 
 # Cron details
@@ -16,6 +16,14 @@ no implementation on names
 5 character cron
 Like -> * * * * *
 
+# The clever part
+use bitmask to store complex checks and use bit shifts to quickly evalute
+For example:
+for minutes 0..59 becomes:
+111111111111111111111111111111111111111111111111111111111111 -> (about 60)
+Every minute allowed
+Shift it left like (1 << 60) - 1
+magic happens
 
 # Extensions
 1. Implement @yearly, @monthly, @daily -> Need to reason when servers
