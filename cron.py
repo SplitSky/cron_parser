4143239163c07ab40f15c74cfe4a6b60b7b0fd71
SplitSky <opop.neska60@gmail.com>
2026-02-09 17:12:00 +0000

    experimenting with bit masks

--- cron.py
+++ cron.py
@@ -1,29 +1,32 @@
 from datetime import datetime
 from typing import Iterable
 
+
 class CronSpec:
     __slots__ = ("minute", "hour", "day", "month", "weekday")
 
     def __init__(self, expression: str):
         fields = expression.split()
         if len(fields) != 5:
-            raise ValueError("Cron expressions must have 5 fields. Wrong format")
-        self.minute = self.parse_field(fields[0],0,59)
+            raise ValueError(
+                "Cron expressions must have 5 fields. Wrong format")
+        self.minute = self.parse_field(fields[0], 0, 59)
         self.hour = self.parse_field(fields[1], 0, 23)
         self.day = self.parse_field(fields[2], 1, 31)
         self.month = self.parse_field(fields[3], 1, 12)
         self.weekday = self.parse_field(fields[4], 0, 6)
 
     def matches(self, dt: datetime) -> bool:
-        cron_weekday = (dt.weekday() + 1) % 7 # lil bit of modulus to fix the range issue
+        # lil bit of modulus to fix the range issue
+        cron_weekday = (dt.weekday() + 1) % 7
         return bool((self.minute >> dt.minute) & 1 and (self.hour >> dt.hour) & 1 and (self.day >> dt.day) & 1 and (self.month >> dt.month) & 1 and (self.weekday >> cron_weekday) & 1)
-    
+
     def parse_field(self, field: str, min: int, max: int) -> int:
         mask = 0
         for part in field.split(","):
             if part == "*":
                 return ((1 << (max - min + 1)) - 1) << min
-            
+
             step = 1
             if "/" in part:
                 part, step_str = part.split("/")
@@ -47,6 +50,7 @@ class CronSpec:
 
         return mask
 
+
 class CronDate:
 
     __slots__ = ("minute", "hour", "day", "month", "weekday", "date")
@@ -68,11 +72,4 @@ class CronDate:
         year = date_out.year
         # find next year
 
-
-
         return date_out
-        
-
-
-
-
--- notes.py
+++ notes.py
@@ -1,23 +1,82 @@
-number = 0
-bin_number = bin(number)
+
 
 def super_print(number: int):
-    print(number)
-    print(str(bin(number))[2:])
+    print(f"number = {number} - {bin(number)}")
+    # print(f"binary = {str(bin(number))[2:]}")
 
-# setting a number on a mask
-# 1. Set all to on
-number = (1 << 60) - 1
-super_print(number)
 
-# 2. reset and set a range on
-number = 0
-for shift in range(10, 21):
-    number |= 1 << shift
-super_print(number)
+def ctz(x: int) -> int:
+    return (x & -x).bit_length() - 1
 
-# 3. reset and set single digits
-number = 0
-for shift in [1,12,23]:
-    number |= 1 << shift
-super_print(number)
+
+def next_allowed_date(A: int, B: int, n_bits: int) -> int:
+    if A <= 0 or (A & (A - 1)) != 0:
+        raise ValueError("A must have exactly one bit set (power of two).")
+
+    if n_bits <= 0:
+        raise ValueError("n_bits must be positive.")
+
+    max_bit = n_bits
+    mask_high = (1 << (max_bit + 1)) - 1
+    if A & ~mask_high:
+        raise ValueError("A is outside the allowed n_bits range (offset=1).")
+    B &= mask_high
+
+    i = ctz(A)
+
+    # Include today: clear bits [0..i-1]
+    if i == 0:
+        lower_mask = 0
+    else:
+        lower_mask = (1 << i) - 1
+    future = B & ~lower_mask
+
+    if future == 0:
+        raise ValueError("Overflow")
+
+    next_index = ctz(future & -future)
+    day = next_index  # offset=1
+    if not (1 <= day <= n_bits):
+        raise ValueError("out of bounds")
+    return int(day)
+
+
+def main():
+    number = 0
+    bin_number = bin(number)
+    # setting a number on a mask
+    # 1. Set all to on
+    number = (1 << 60) - 1
+    # super_print(number)
+
+    # 2. reset and set a range on
+    number = 0
+    for shift in range(10, 21):
+        number |= 1 << shift
+    # super_print(number)
+
+    # 3. reset and set single digits
+    number = 0
+    for shift in [1, 12, 23]:
+        number |= 1 << shift
+    # super_print(number)
+
+    number = 0
+    for shift in [1, 5, 12, 23]:
+        number |= 1 << shift
+    # super_print(number)
+    d = 5
+    d_mask = 1 << d
+    # super_print(d_mask)
+
+    number1 = 21
+    super_print(number1)
+    number2 = bin(number1)
+    number2 = -number2
+    number2 = int(number2)
+    super_print(number2)
+    number3 = ~number1
+    result = number1 & -number1
+
+
+main()
